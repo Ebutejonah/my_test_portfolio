af5c9eddca9959fc91bb877af4f31a27fc6f4a12
giftedmatrix <ebutejonah386@gmail.com>
2022-12-14 09:20:17 +0100

    heroku settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,3 +129,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
