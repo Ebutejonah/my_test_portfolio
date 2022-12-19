6010a218c3f7996535df9b8c1294b66ccbb33020
giftedmatrix <ebutejonah386@gmail.com>
2022-12-19 22:32:50 +0100

    setting up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./new/templates/home/*.html',"./new/static/stylesheet2.css","./new/static/main.js"],
+  content: ['./home/templates/home/*.html',"./home/static/stylesheet2.css","./home/static/main.js"],
   theme: {
     extend: {},
   },
