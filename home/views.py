8de9d8a92fccb568ba7c450e613720b6bbe31b54
EBUTE JONAH <ebutejonah386@gmail.com>
2022-12-19 20:35:51 +0100

    Update views.py

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from .models import Home,Review
 
 
 def home_view(request):
-    obj=Home.objects.all()
+    obj=Home.objects.get(id=2)
     form=RawReviewForm() 
     if request.method=='POST':
         form=RawReviewForm(request.POST or None)
@@ -15,21 +15,21 @@ def home_view(request):
     return render(request,'home/home.html',context)
 
 def portfolio_view(request):
-    obj=Home.objects.all()
+    obj=Home.objects.get(id=2)
     context={'object':obj}
     return render(request,"home/portfolio.html",context)
 
 def webdesign_view(request):
-    obj=Home.objects.all()
+    obj=Home.objects.get(id=2)
     context={'object':obj}
     return render(request,"home/web_design.html",context)
 
 def machinelearning_view(request):
-    obj=Home.objects.all()
+    obj=Home.objects.get(id=2)
     context={'object':obj}
     return render(request,"home/machine_learn.html",context)
 
 def appdev_view(request):
-    obj=Home.objects.all()
+    obj=Home.objects.get(id=2)
     context={'object':obj}
     return render(request,"home/app_dev.html",context)
