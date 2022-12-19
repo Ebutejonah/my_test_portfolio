2fdbf8fc827cd139d652bcc074f8134e19d5f226
giftedmatrix <ebutejonah386@gmail.com>
2022-12-19 22:22:45 +0100

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./new/templates/home/*.html',"./new/static/stylesheet2.css","./new/static/main.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'media',
+}
