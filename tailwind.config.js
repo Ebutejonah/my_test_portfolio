dfe53da16b56c688f8a8b66183c245bc540e7b68
giftedmatrix <ebutejonah386@gmail.com>
2022-12-20 14:40:50 +0100

    removed tailwindcss

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./home/templates/home/*.html',"./home/static/stylesheet2.css","./home/static/main.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  darkMode: 'media',
-}
