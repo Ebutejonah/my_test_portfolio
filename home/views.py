eed5f3112ec79a556a1ad604246f968b822f491d
giftedmatrix <ebutejonah386@gmail.com>
2022-12-22 04:05:01 +0100

    html

--- home/views.py
+++ home/views.py
@@ -12,24 +12,24 @@ def home_view(request):
             Review.objects.create(**form.cleaned_data)
             form=RawReviewForm()
     context={'object':obj, 'form':form}
-    return render(request,'home/home.html',context)
+    return render(request,'home.html',context)
 
 def portfolio_view(request):
     obj=Home.objects.get(id=2)
     context={'object':obj}
-    return render(request,"home/portfolio.html",context)
+    return render(request,"portfolio.html",context)
 
 def webdesign_view(request):
     obj=Home.objects.get(id=2)
     context={'object':obj}
-    return render(request,"home/web_design.html",context)
+    return render(request,"web_design.html",context)
 
 def machinelearning_view(request):
     obj=Home.objects.get(id=2)
     context={'object':obj}
-    return render(request,"home/machine_learn.html",context)
+    return render(request,"machine_learn.html",context)
 
 def appdev_view(request):
     obj=Home.objects.get(id=2)
     context={'object':obj}
-    return render(request,"home/app_dev.html",context)
+    return render(request,"app_dev.html",context)
