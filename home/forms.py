60288dcef57f528fe7034a913021d1e21ee4fc5b
giftedmatrix <ebutejonah386@gmail.com>
2022-12-26 16:23:29 +0100

    .

--- home/forms.py
+++ home/forms.py
@@ -9,6 +9,6 @@ class ReviewForm(forms.ModelForm):
         fields= ['feedback']
 
 class RawReviewForm(forms.Form):
-    feedback= forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':50, 'style':'resize:none;font-family:Arial;font-size:2rem;background:white;padding:7px;'}))
+    feedback= forms.CharField(widget=forms.Textarea(attrs={'rows':10, 'cols':50, 'style':'resize:none;font-family:Arial, sans-serif;font-size:1rem;background:white;padding:10px;'}))
 
     
